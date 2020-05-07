8eb2b712a28c68d429c0c0e0d1582c3f8005fc8e
NataliaEspinola <naespinola@miuandes.cl>
2020-05-07 19:59:58 -0400

    no tables again

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :wall_of_comment
 
-  has_many :received_comment, :class_name => 'RepliesToComment', :foreign_key => 'comment_receiver_id'
-  has_many :transmitted_comment, :class_name => 'RepliesToComment', :foreign_key => 'comment_transmitter_id'
+  has_many :received_comments, :class_name => 'RepliesToComment', :foreign_key => 'comment_receiver_id'
+  has_many :transmitted_comments, :class_name => 'RepliesToComment', :foreign_key => 'comment_transmitter_id'
 
   validates :image, allow_blank: true, format: {
       with: %r{\.(gif|jpg|png)\Z}i,

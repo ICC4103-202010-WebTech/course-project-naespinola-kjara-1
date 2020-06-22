98048333b761e6daa2a05df7b920d26cf6332259
kjcblack <kjara@miuandes.cl>
2020-06-22 13:09:28 -0400

    profile ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @user = User.last
+    @user =  User.find_by(id: current_person.id)
 
   end
 
@@ -69,7 +69,7 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      @user = User.find_by(id: current_person.id)
     end
 
     # Only allow a list of trusted parameters through.
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ApplicationRecord
   #validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
-  validates :password, presence: true,  :length => { :minimum => 6 }
+  # validates :password, presence: true,  :length => { :minimum => 6 }
   validates :in_blacklist, inclusion: { in: [true, false] }
 
   validates :image, allow_blank: true, format: {

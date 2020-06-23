49eeb94f1a054a38228b775f2f0f9d83822522df
kjcblack <kjara@miuandes.cl>
2020-06-23 01:06:54 -0400

    see profile ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     #ra @user = User.all
-    # @user = User.all.where("users.id=#{params[:id]} ")
+    # @user = User.where("users.id=#{params[:id]} ")
       # @user = User.select("users.id= #{params[:id]}")
 
   end

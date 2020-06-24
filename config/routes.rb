173162055a07e14f8d41c3741bbb8fb751f95605
kjcblack <kjara@miuandes.cl>
2020-06-23 23:09:39 -0400

    change password in profile OK

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,8 @@ Rails.application.routes.draw do
 
 
 
-  devise_for :users, :controllers =>{ :omniauth_callbacks => "omniauth_callbacks" } , :skip => [:registrations]
-  as :user do
-    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
-    put 'users' => 'devise/registrations#update', :as => 'user_registration'
-  end
+  devise_for :users, :controllers =>{ :omniauth_callbacks => "omniauth_callbacks" }
+
   devise_for :admins, controllers: { sessions: 'admins/sessions' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "pages#home"

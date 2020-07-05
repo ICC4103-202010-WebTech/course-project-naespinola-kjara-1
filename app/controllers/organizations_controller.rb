dd1bbb913a2b4be2a502e17d215d27db85176339
NataliaEspinola <naespinola@miuandes.cl>
2020-07-05 01:20:21 -0400

    admin can delete all

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -65,7 +65,7 @@ class OrganizationsController < ApplicationController
 
     @organization.destroy
     respond_to do |format|
-      format.html { redirect_to organizations_url, notice: 'Organization was successfully destroyed.' }
+      format.html { redirect_to root_path, notice: 'Organization was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
